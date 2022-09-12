080ad1c5eeacdce964a61bc36d6b8acbf1d6c937
a-ti-k <63835331+a-ti-k@users.noreply.github.com>
2022-09-12 09:00:06 +0600

    Create linearSearch.py

---
+++ linearSearch.py
@@ -0,0 +1,14 @@
+arr = []
+n = int(input("enter the number of elements:"))
+for i in range(0,n):
+    ele = int(input())
+    arr.append(ele)
+
+def linearSearch(arr,find):
+    for i in range (len(arr)):
+        if find == arr[i]:
+            return i+1
+
+find =int(input("need to find:"))
+
+print("array after sorting:",linearSearch(arr,find,))
